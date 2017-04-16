0148ca98cdcdcf1e6a367e5b91bbf3fdc0f97ec6
leohentschker <leohentschker@college.harvard.edu>
2017-04-16 18:56:22 -0400

    working custom command running on install via pip

--- setup.py
+++ setup.py
@@ -1,4 +1,10 @@
+from setuptools.command.install import install
 from distutils.core import setup
+
+
+class PybluezInstall(install):
+    def run(self):
+        install.run(self)
 
 setup(
     name='VillageReach',
@@ -12,4 +18,5 @@ setup(
     description='Facilitating file transfer between drones and basis',
     long_description=open('README.md').read(),
     install_requires=[],
+    cmdclass={"install": PybluezInstall}
 )
